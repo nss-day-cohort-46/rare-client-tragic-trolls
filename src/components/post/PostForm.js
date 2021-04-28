d4e652fa83bc3e7497a6911de3224dc26744ee11
Nick Carver <nicholascarver2@outlook.com>
2021-04-28 15:27:34 -0500

    imported form components from reactstrap and copied a form from kennels

---
+++ src/components/post/PostForm.js
@@ -0,0 +1,144 @@
+import React, { useContext, useEffect, useState } from "react"
+import "./Post.css"
+import { useHistory, useParams } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+export const AnimalForm = () => {
+    const { addAnimal, getAnimalById, updateAnimal } = useContext(AnimalContext)
+    const { locations, getLocations } = useContext(LocationContext)
+    const { customers, getCustomers } = useContext(CustomerContext)
+
+    //for edit, hold on to state of animal in this view
+    const [animal, setAnimal] = useState({
+        name: "",
+        breed: "",
+        customerId: 0,
+        locationId: 0
+    })
+
+    //wait for data before button is active. Look at the button to see how it's setting itself to disabled or not based on this state
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Now that the form can be used for editing as well as adding an animal, you need access to the animal id for fetching the animal you want to edit
+    const { animalId } = useParams();
+    const history = useHistory();
+
+    //when field changes, update state. This causes a re-render and updates the view.
+    //Controlled component
+    const handleControlledInputChange = (event) => {
+        //When changing a state object or array,
+        //always create a copy make changes, and then set state.
+        const newAnimal = { ...animal }
+        //animal is an object with properties.
+        //set the property to the new value
+        newAnimal[event.target.id] = event.target.value
+        //update state
+        setAnimal(newAnimal)
+    }
+
+    const handleSaveAnimal = () => {
+        if (parseInt(animal.locationId) === 0) {
+            window.alert("Please select a location")
+        } else {
+            //disable the button - no extra clicks
+            setIsLoading(true);
+            // This is how we check for whether the form is being used for editing or creating. If the URL that got us here has an id number in it, we know we want to update an existing record of an animal
+            if (animalId) {
+                //PUT - update
+                updateAnimal({
+                    id: animal.id,
+                    name: animal.name,
+                    breed: animal.breed,
+                    locationId: parseInt(animal.locationId),
+                    customerId: parseInt(animal.customerId)
+                })
+                    .then(() => history.push(`/animals/detail/${animal.id}`))
+            } else {
+                //POST - add
+                addAnimal({
+                    name: animal.name,
+                    breed: animal.breed,
+                    locationId: parseInt(animal.locationId),
+                    customerId: parseInt(animal.customerId)
+                })
+                    .then(() => history.push("/animals"))
+            }
+        }
+    }
+
+    const handleCancel = (event) => {
+        event.preventDefault()
+        history.goBack()
+    }
+
+
+    // Get customers and locations. If animalId is in the URL, getAnimalById
+    useEffect(() => {
+        getCustomers().then(getLocations).then(() => {
+            if (animalId) {
+                getAnimalById(animalId)
+                    .then(animal => {
+                        setAnimal(animal)
+                        setIsLoading(false)
+                    })
+            } else {
+                setIsLoading(false)
+            }
+        })
+    }, [])
+
+    return (
+        <form className="animalForm">
+            <h2 className="animalForm__title">{animalId ? "Edit Animal" : "Add Animal"}</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="animalName">Animal name: </label>
+                    <input type="text" id="name" required autoFocus className="form-control"
+                        placeholder="Animal name"
+                        onChange={handleControlledInputChange}
+                        value={animal.name} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="breed">Animal breed:</label>
+                    <input type="text" id="breed" onChange={handleControlledInputChange} required className="form-control" placeholder="Animal breed" value={animal.breed} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="location">Assign to location: </label>
+                    <select value={animal.locationId} id="locationId" className="form-control" onChange={handleControlledInputChange}>
+                        <option value="0">Select a location</option>
+                        {locations.map(l => (
+                            <option key={l.id} value={l.id}>
+                                {l.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="customer">Customer: </label>
+                    <select value={animal.customerId} id="customerId" className="form-control" onChange={handleControlledInputChange}>
+                        <option value="0">Select a customer</option>
+                        {customers.map(c => (
+                            <option key={c.id} value={c.id}>
+                                {c.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </fieldset>
+            <button className="btn btn-primary"
+                disabled={isLoading}
+                onClick={event => {
+                    event.preventDefault() // Prevent browser from submitting the form and refreshing the page
+                    handleSaveAnimal()
+                }}>
+                {animalId ? "Submit Edit" : "Save New Animal"}</button>
+            <button className="btn" onClick={handleCancel}>Cancel</button>
+        </form>
+    )
+}
