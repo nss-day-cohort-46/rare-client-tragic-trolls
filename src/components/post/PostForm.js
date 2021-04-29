4666024f1b7d005fb68ae0eef0044e1af6839c73
Nick Carver <nicholascarver2@outlook.com>
2021-04-29 10:28:21 -0500

    got the create post to work

--- src/components/post/PostForm.js
+++ src/components/post/PostForm.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react"
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import { PostContext } from "./PostProvider"
 
 export const PostForm = (props) => {
@@ -15,47 +15,64 @@ export const PostForm = (props) => {
         "publication_date": currentDate,
         "image_url": "",
         "content": "",
-        "approved": false
+        "approved": false,
+        "tag_ids": []
     })
 
     const handleControlledInputChange = (event) => {
         let newPost = { ...post }
-        newPost[event.target.id] = event.target.value
-        setPost(newPost)
+        if (event.target.name == "tag_ids") {
+            const tag = parseInt(event.target.value)
+            const tagIndex = post.tag_ids.indexOf(tag)
+            if (tagIndex > -1) {
+                post.tag_ids.splice(tagIndex, 1)
+            } else {
+                post.tag_ids.push(tag)
+            }
+        }
+        else if (event.target.id == "category_id") {
+            newPost[event.target.id] = parseInt(event.target.value)
+            setPost(newPost)    
+        }
+        else {
+            newPost[event.target.id] = event.target.value
+            setPost(newPost)
+        }
+        console.log(newPost)
     }
 
-    const handleSubmitClick = (event) => {
-        createPost(post)
-    }
+        const handleSubmitClick = (event) => {
+            createPost(post)
+        }
 
-    return (
-        <Form className="postForm" autoComplete="on">
-            <h2 className="postForm__title">New Post</h2>
-            <fieldset>
-                <FormGroup>
-                    <Label for="postTitle">Title</Label>
-                    <Input onChange={handleControlledInputChange} type="text" name="title" id="title" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="postCategory">Category</Label>
-                    <Input onChange={handleControlledInputChange} type="select" name="selectCategory" id="category_id">
-                        <option value="0">choose a category...</option>
-                        <option value="1">Category One</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="postContent">Your Thoughts</Label>
-                    <Input type="textarea" name="text" id="content" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="postImageURL">URL for an Image</Label>
-                    <Input onChange={handleControlledInputChange} type="text" name="imageURL" id="image_url" />
-                </FormGroup>
-                <FormGroup check>
-                    <Label check><Input type="checkbox" />Check me out</Label>
-                </FormGroup>
-            </fieldset>
-            <Button className="btn__submit" onClick={handleSubmitClick}>Submit</Button>
-        </Form>
-    )
-}
+        return (
+            <Form className="postForm" autoComplete="on">
+                <h2 className="postForm__title">New Post</h2>
+                <fieldset>
+                    <FormGroup>
+                        <Label for="postTitle">Title</Label>
+                        <Input onChange={handleControlledInputChange} type="text" name="title" id="title" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="postCategory">Category</Label>
+                        <Input onChange={handleControlledInputChange} type="select" name="selectCategory" id="category_id">
+                            <option value="0">choose a category...</option>
+                            <option value="1">Category One</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="postContent">Your Thoughts</Label>
+                        <Input onChange={handleControlledInputChange} type="textarea" name="text" id="content" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="postImageURL">URL for an Image</Label>
+                        <Input onChange={handleControlledInputChange} type="text" name="imageURL" id="image_url" />
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check><Input name="tag_ids" value={1} onChange={handleControlledInputChange} type="checkbox" />Tag #1</Label>
+                    </FormGroup>
+                </fieldset>
+                <Button className="btn__submit" onClick={handleSubmitClick}>Submit</Button>
+            </Form>
+        )
+    }
