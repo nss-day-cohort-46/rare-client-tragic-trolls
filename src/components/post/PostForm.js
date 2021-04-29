ad2218ef2358eb7425b0525333621090b605e0f4
Nick Carver <nicholascarver2@outlook.com>
2021-04-29 09:29:44 -0500

    starting setting up the post form

--- src/components/post/PostForm.js
+++ src/components/post/PostForm.js
@@ -1,144 +1,120 @@
 import React, { useContext, useEffect, useState } from "react"
-import "./Post.css"
-import { useHistory, useParams } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { CommentContext } from "./CommentProvider"
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
 
-export const AnimalForm = () => {
-    const { addAnimal, getAnimalById, updateAnimal } = useContext(AnimalContext)
-    const { locations, getLocations } = useContext(LocationContext)
-    const { customers, getCustomers } = useContext(CustomerContext)
+export const PostForm = (props) => {
+    const { createComment } = useContext(CommentContext)
 
-    //for edit, hold on to state of animal in this view
-    const [animal, setAnimal] = useState({
-        name: "",
-        breed: "",
-        customerId: 0,
-        locationId: 0
+    const currentUser = parseInt(localStorage.getItem("rare_user_id"))
+    const today = new Date()
+    const currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+    const [post, setPost] = useState({
+        "user_id": currentUser,
+        "category_id": 0,
+        "title": 0,
+        "publication_date": currentDate,
+        "image_url": "",
+        "content": "",
+        "approved": false
     })
 
-    //wait for data before button is active. Look at the button to see how it's setting itself to disabled or not based on this state
-    const [isLoading, setIsLoading] = useState(true);
-
-    // Now that the form can be used for editing as well as adding an animal, you need access to the animal id for fetching the animal you want to edit
-    const { animalId } = useParams();
-    const history = useHistory();
-
-    //when field changes, update state. This causes a re-render and updates the view.
-    //Controlled component
     const handleControlledInputChange = (event) => {
-        //When changing a state object or array,
-        //always create a copy make changes, and then set state.
-        const newAnimal = { ...animal }
-        //animal is an object with properties.
-        //set the property to the new value
-        newAnimal[event.target.id] = event.target.value
-        //update state
-        setAnimal(newAnimal)
+        let newComment = { ...post }
+
+        newComment[event.target.id] = event.target.value
+
+        setPost(newComment)
     }
 
-    const handleSaveAnimal = () => {
-        if (parseInt(animal.locationId) === 0) {
-            window.alert("Please select a location")
-        } else {
-            //disable the button - no extra clicks
-            setIsLoading(true);
-            // This is how we check for whether the form is being used for editing or creating. If the URL that got us here has an id number in it, we know we want to update an existing record of an animal
-            if (animalId) {
-                //PUT - update
-                updateAnimal({
-                    id: animal.id,
-                    name: animal.name,
-                    breed: animal.breed,
-                    locationId: parseInt(animal.locationId),
-                    customerId: parseInt(animal.customerId)
-                })
-                    .then(() => history.push(`/animals/detail/${animal.id}`))
-            } else {
-                //POST - add
-                addAnimal({
-                    name: animal.name,
-                    breed: animal.breed,
-                    locationId: parseInt(animal.locationId),
-                    customerId: parseInt(animal.customerId)
-                })
-                    .then(() => history.push("/animals"))
-            }
-        }
+    const handleSubmitClick = (event) => {
+        createComment(post)
     }
-
-    const handleCancel = (event) => {
-        event.preventDefault()
-        history.goBack()
-    }
-
-
-    // Get customers and locations. If animalId is in the URL, getAnimalById
-    useEffect(() => {
-        getCustomers().then(getLocations).then(() => {
-            if (animalId) {
-                getAnimalById(animalId)
-                    .then(animal => {
-                        setAnimal(animal)
-                        setIsLoading(false)
-                    })
-            } else {
-                setIsLoading(false)
-            }
-        })
-    }, [])
 
     return (
-        <form className="animalForm">
-            <h2 className="animalForm__title">{animalId ? "Edit Animal" : "Add Animal"}</h2>
+        <Form className="postForm" autoComplete="on">
+            <h2 className="postForm__title">New Post</h2>
             <fieldset>
-                <div className="form-group">
-                    <label htmlFor="animalName">Animal name: </label>
-                    <input type="text" id="name" required autoFocus className="form-control"
-                        placeholder="Animal name"
-                        onChange={handleControlledInputChange}
-                        value={animal.name} />
-                </div>
+                <FormGroup>
+                    <Label for="postTitle">Title</Label>
+                    <Input type="textarea" name="title" id="title" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="postCategory">Select</Label>
+                    <Input type="select" name="selectCategory" id="category_id">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Input>
+                </FormGroup>
             </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="breed">Animal breed:</label>
-                    <input type="text" id="breed" onChange={handleControlledInputChange} required className="form-control" placeholder="Animal breed" value={animal.breed} />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="location">Assign to location: </label>
-                    <select value={animal.locationId} id="locationId" className="form-control" onChange={handleControlledInputChange}>
-                        <option value="0">Select a location</option>
-                        {locations.map(l => (
-                            <option key={l.id} value={l.id}>
-                                {l.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="customer">Customer: </label>
-                    <select value={animal.customerId} id="customerId" className="form-control" onChange={handleControlledInputChange}>
-                        <option value="0">Select a customer</option>
-                        {customers.map(c => (
-                            <option key={c.id} value={c.id}>
-                                {c.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </fieldset>
-            <button className="btn btn-primary"
-                disabled={isLoading}
-                onClick={event => {
-                    event.preventDefault() // Prevent browser from submitting the form and refreshing the page
-                    handleSaveAnimal()
-                }}>
-                {animalId ? "Submit Edit" : "Save New Animal"}</button>
-            <button className="btn" onClick={handleCancel}>Cancel</button>
-        </form>
+            <Button className="btn__submit" onClick={handleSubmitClick}>Submit</Button>
+        </Form>
     )
 }
+
+const Example = (props) => {
+    return (
+        <Form>
+            <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="exampleSelectMulti">Select Multiple</Label>
+                <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </Input>
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="exampleFile">File</Label>
+                <Input type="file" name="file" id="exampleFile" />
+                <FormText color="muted">
+                    This is some placeholder block-level help text for the above input.
+                    It's a bit lighter and easily wraps to a new line.
+        </FormText>
+            </FormGroup>
+            <FormGroup tag="fieldset">
+                <legend>Radio Buttons</legend>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="radio" name="radio1" />{' '}
+            Option one is this and that—be sure to include why it's great
+          </Label>
+                </FormGroup>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="radio" name="radio1" />{' '}
+            Option two can be something else and selecting it will deselect option one
+          </Label>
+                </FormGroup>
+                <FormGroup check disabled>
+                    <Label check>
+                        <Input type="radio" name="radio1" disabled />{' '}
+            Option three is disabled
+          </Label>
+                </FormGroup>
+            </FormGroup>
+            <FormGroup check>
+                <Label check>
+                    <Input type="checkbox" />{' '}
+          Check me out
+        </Label>
+            </FormGroup>
+            <Button>Submit</Button>
+        </Form>
+    );
+}
+
+export default Example;
